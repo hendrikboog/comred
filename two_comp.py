db73aef8fb41e39f298823d17822bfe314ea8a0e
Hendrik Boog <hendrik.boog@mr.mpg.de>
2021-02-03 17:05:56 +0100

    Update to version 0.2 - Added data simulation script and updated files accordingly.

--- two_comp.py
+++ two_comp.py
@@ -203,7 +203,7 @@ def invisplot():
 # -----------------------------------------------------------------------------
 
 def make_comparison_boxplots():
-    description = ("ComRed 0.1.1 - Last update: 2020-12-15 - Script for "
+    description = ("ComRed 0.2 - Last update: 2021-02-03 - Script for "
                    "comparing two sets of receptor datasets and produce "
                    "boxplot figures")
     parser = argparse.ArgumentParser(description=description)
